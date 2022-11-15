6a8e28feb004c6d6fa252f1947d272b530a62286
IronJin <85132068+IronJin@users.noreply.github.com>
2022-11-15 20:56:11 +0900

    yml 수정

--- src/main/java/challenges/challenges/controller/member/EmailRequestDTO.java
+++ src/main/java/challenges/challenges/controller/member/EmailRequestDTO.java
@@ -3,12 +3,14 @@ package challenges.challenges.controller.member;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 @Getter @Setter
 public class EmailRequestDTO {
 
     @NotEmpty
+    @Email
     private String m_email; //{"m_email":"이메일주소"}
 
 }
